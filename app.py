a43ccbb5721197ef393c7a6c54173fa6eef936b2
Chitraksh Sharma <chitraksh73278@gmail.com>
2025-07-29 18:32:15 +0530

    Make and revert changes

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def predict():
     return render_template('form.html', form = form, prediction = prediction, pred_tips1 = pred_tips1, pred_tips2 = pred_tips2, pred_tips3 = pred_tips3)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(host = "0.0.0.0", port = 8080)
